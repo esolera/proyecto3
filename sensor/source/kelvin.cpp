d23cef614189af246673ade28874126966f8b474
juansoto49 <33875688+juansoto49@users.noreply.github.com>
2018-08-04 00:49:24 -0600

    Changes to Kelvin

--- sensor/source/kelvin.cpp
+++ sensor/source/kelvin.cpp
@@ -4,7 +4,7 @@
 void kelvin::processing()                         // Describe time-domain behaviour
 {
   double a =(double) in.read();
-  double salida = (a*0.03257+3.3224)-273;
+  double salida = (a*0.03257+3.3224)+273;
   sc_dt::sc_int<NBitsADC> salida2 = lround(salida);
   out.write(salida2);                                      // Write sample to the output
 }
